5ea8cd92ec92bed8291454ac69914a55712a3a00
m4t3u2 <ms.mateus@hotmail.com>
2021-06-20 21:09:52 -0300

    User service.

--- src/main/java/blog/controllers/UserController.java
+++ src/main/java/blog/controllers/UserController.java
@@ -1,14 +1,13 @@
 package blog.controllers;
 
-import java.util.HashSet;
+import java.net.URI;
 import java.util.List;
-import java.util.Set;
 
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,87 +15,43 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import blog.models.ERole;
-import blog.models.Role;
 import blog.models.User;
 import blog.payload.request.SignupRequest;
-import blog.payload.response.MessageResponse;
-import blog.repository.role.RoleRepository;
-import blog.repository.user.UserRepository;
+import blog.services.UserService;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("/api/user")
 public class UserController {
-	@Autowired
-	UserRepository userRepository;
 
 	@Autowired
-	RoleRepository roleRepository;
-
-	@Autowired
-	PasswordEncoder encoder;
+	UserService userService;
 
 	@PostMapping
-	public ResponseEntity<?> cadastrar(@Valid @RequestBody SignupRequest signUpRequest) {
-		if (userRepository.existsByUsername(signUpRequest.getUsername())) {
-			return ResponseEntity.badRequest().body(new MessageResponse("Erro: Usuário já existe!"));
-		}
+	public ResponseEntity<Void> cadastrar(@Valid @RequestBody SignupRequest signUpRequest) {
 
-		if (userRepository.existsByEmail(signUpRequest.getEmail())) {
-			return ResponseEntity.badRequest().body(new MessageResponse("Erro: E-mail já está em uso!"));
-		}
+		User cadastrado = userService.cadastrar(signUpRequest);
 
-		User user = new User(signUpRequest.getName(), signUpRequest.getUsername(), signUpRequest.getEmail(),
-				encoder.encode(signUpRequest.getPassword()));
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(cadastrado.getId())
+				.toUri();
 
-		Set<String> strRoles = signUpRequest.getRole();
-		Set<Role> roles = new HashSet<>();
-
-		if (strRoles == null) {
-			Role userRole = roleRepository.findByName(ERole.ROLE_USER)
-					.orElseThrow(() -> new RuntimeException("Erro: Permissão não encontrada."));
-			roles.add(userRole);
-		} else {
-			strRoles.forEach(role -> {
-				switch (role) {
-				case "admin":
-					Role adminRole = roleRepository.findByName(ERole.ROLE_ADMIN)
-							.orElseThrow(() -> new RuntimeException("Erro: Permissão não encontrada."));
-					roles.add(adminRole);
-
-					break;
-				case "mod":
-					Role modRole = roleRepository.findByName(ERole.ROLE_MODERATOR)
-							.orElseThrow(() -> new RuntimeException("Erro: Permissão não encontrada."));
-					roles.add(modRole);
-
-					break;
-				default:
-					Role userRole = roleRepository.findByName(ERole.ROLE_USER)
-							.orElseThrow(() -> new RuntimeException("Erro: Permissão não encontrada."));
-					roles.add(userRole);
-				}
-			});
-		}
-
-		user.setRoles(roles);
-		userRepository.save(user);
-
-		return ResponseEntity.ok(new MessageResponse("Usuário registrado!"));
+		return ResponseEntity.created(uri).build();
 	}
 
 	@GetMapping("/{id}")
 	public ResponseEntity<User> buscar(@PathVariable("id") long id) {
-		// TODO Tratar quando não tem nenhum encontrado.
-		return ResponseEntity.ok(userRepository.findById(id).get());
+		User user = userService.buscar(id);
+
+		return ResponseEntity.status(HttpStatus.OK).body(user);
 	}
 
 	@GetMapping
 	public ResponseEntity<List<User>> buscarTodos() {
-		// TODO Tratar quando não tem nenhum encontrado.
-		return ResponseEntity.ok(userRepository.findAll());
+		List<User> users = userService.buscarTodos();
+
+		return ResponseEntity.ok().body(users);
 	}
 
 }
